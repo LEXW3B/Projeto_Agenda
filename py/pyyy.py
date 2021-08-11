c94027c6e1a07c3a221817a5dea4a47bc93c98b7
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-11 06:58:31 -0300

    Update pyyy.py

--- py/pyyy.py
+++ py/pyyy.py
@@ -16,7 +16,19 @@
 #    print(f'{k} = {v}')
 
 
+           #novo
 
+aluno =dict()
+aluno['nome']=str(input('nome: '))
+aluno['media']=float(input(f'media de {aluno["aluno"]}:'))
+if aluno['media']>=7:
+    aluno['situação']='Aprovado'
+elif 5<= aluno ['media']<7:
+    aluno['situação']='Recuperação'
+else:
+    aluno['situação']='Reprovado'
+for k,v in aluno.items():
+    print(f'{k} é igual a {v}')
 
 
 
