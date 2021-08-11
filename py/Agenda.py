f6e696d1f882959ea15dd1878c3dfc16ac04da08
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-10 22:55:54 -0300

    Update Agenda.py

--- py/Agenda.py
+++ py/Agenda.py
@@ -1,33 +1,48 @@
 #Projeto Agenda para a faculdade 
-print('------------------------------------------------')
-print('=====================AGENDA=====================')
-print('------------------------------------------------')
-print('')
-print('Para adicionar contato tecle-----------------[1]')
-print('Para buscar nomes existentes tecle-----------[2]')
-print('Para alterar contatos tecle------------------[3]')
-print('Para remover contato tecle-------------------[4]')
-print('')
-print('------------------------------------------------')
-print('')#PRONTO
+# print('------------------------------------------------')
+# print('=====================AGENDA=====================')
+# print('------------------------------------------------')
+# print('')
+# print('Para adicionar contato tecle-----------------[1]')
+# print('Para buscar nomes existentes tecle-----------[2]')
+# print('Para alterar contatos tecle------------------[3]')
+# print('Para remover contato tecle-------------------[4]')
+# print('')
+# print('------------------------------------------------')
+# print('')#PRONTO
 
-#INPUT === PRONTO
-input("Digite seu nome: ")
-input("Digite seu telefone: ")
-input("Digite seu e-mail: ")
-input("Digite seu usuário no Instagram: ")
-input("Digite seu usuário no Twitter: ")
+           #INPUT === PRONTO
+nome=str(input("Digite seu nome: "))
+tel=int(input("Digite seu telefone: "))
+email=str(input("Digite seu e-mail: "))
+insta=str(input("Digite seu usuário no Instagram: "))
+twi=str(input("Digite seu usuário no Twitter: "))
+
+if(nome===str){
+    print(nome)
+}else{
+    nome=str(input("Digite seu nome: ")) 
+}
 
 #BIBLIOTECA
+lista=[]
+contato1={'nome': 'Alexandre', 'telefone': '85-9898-0000', 'email': 'alexandre@serv.com', 'instagram': 'alex123', 'twitter': '@alex321'}
+
+contato2={'nome': 'Kelly', 'telefone': '85-9898-0001', 'email': 'kelly@serv.com', 'instagram': 'keka123', 'twitter': '@keka321'}
+
+contato3={'nome': 'Vanda', 'telefone': '85-9898-0002', 'email': 'vanda@serv.com', 'instagram': 'vanda123', 'twitter': '@vanda321'}
+
+contato4={'nome': 'Ricardo', 'telefone': '85-9898-0003', 'email': 'ricardo@serv.com', 'instagram': 'ricardo123', 'twitter': '@ricardo321'}
+
+print(lista)
 
 
 
-
-# nome = 'Alexandre', 'Kelly', 'Vanda', 'Ricardo'
-# telefone = '85-9898-0000', '85-9898-0001', '85-9898-0002', '85-9898-0003'
-# email = 'alexandre@serv.com', 'kelly@serv.com', 'vanda@serv.com', 'raimundo@serv.com'
-# instagram = 'alex123', 'keka123', 'vanda123', 'raimundo123'
-# twitter = '@alex321', '@keka321', '@vanda321', '@raimundo321'
+#  =   '', ''
+#  =   '85-9898-0002', '85-9898-0003'
+#  =   'vanda@serv.com', 'raimundo@serv.com'
+#  =   'vanda123', 'raimundo123'
+#  = , , '@vanda321', '@raimundo321'
 
 # print(nome)
 # print(telefone)
