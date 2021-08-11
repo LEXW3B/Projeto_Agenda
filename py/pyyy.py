497361b834c33f6493b636380740953131ff33d2
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-10 22:56:03 -0300

    Update pyyy.py

--- py/pyyy.py
+++ py/pyyy.py
@@ -1,4 +1,20 @@
 #biblioteca dicionarios
+#pessoas=()estrutura para uma tupla
+#pessoas=[]estruura para uma lista
+#pessoas={}estrutura para um dicionario
+#pessoas={'nome': 'gustavo', 'sexo': 'M', 'idade': '22'}
+#print=(pessoas.keys())vai aparecer nome, sexo, idade
+#print=(pessoas.values())vai aparecer gustavo, M, 22
+#print(pessoas.items())vai mostar(nome,gustavo),(sexo,M),(idade,22)
+
+
+
+#pessoas={'nome': 'gustavo', 'sexo': 'M', 'idade': '22'}
+#print(pessoas.items())
+
+#for k,v in pessoas.items():#O (k) é keys & o (v) é values
+#    print(f'{k} = {v}')
+
 
 
 
