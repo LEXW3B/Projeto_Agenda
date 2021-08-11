1bc426e134f50f9cf83cf9237ac6f58d7e974b1f
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-10 21:42:18 -0300

    Create Agenda.py

---
+++ py/Agenda.py
@@ -0,0 +1,33 @@
+#Projeto Agenda para a faculdade 
+print('------------------------------------------------')
+print('=====================AGENDA=====================')
+print('------------------------------------------------')
+print('')
+print('Para adicionar contato tecle-----------------[1]')
+print('Para buscar nomes existentes tecle-----------[2]')
+print('Para alterar contatos tecle------------------[3]')
+print('Para remover contato tecle-------------------[4]')
+print('')
+print('------------------------------------------------')
+print('')#PRONTO
+
+#INPUT === PRONTO
+input("Digite seu nome: ")
+input("Digite seu telefone: ")
+input("Digite seu e-mail: ")
+input("Digite seu usuário no Instagram: ")
+input("Digite seu usuário no Twitter: ")
+
+#BIBLIOTECA
+
+
+
+
+# nome = 'Alexandre', 'Kelly', 'Vanda', 'Ricardo'
+# telefone = '85-9898-0000', '85-9898-0001', '85-9898-0002', '85-9898-0003'
+# email = 'alexandre@serv.com', 'kelly@serv.com', 'vanda@serv.com', 'raimundo@serv.com'
+# instagram = 'alex123', 'keka123', 'vanda123', 'raimundo123'
+# twitter = '@alex321', '@keka321', '@vanda321', '@raimundo321'
+
+# print(nome)
+# print(telefone)
