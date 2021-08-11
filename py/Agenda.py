d593d560c4a4a5b29ddb09eeb7e345ef7c6fde48
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-11 06:58:21 -0300

    Update Agenda.py

--- py/Agenda.py
+++ py/Agenda.py
@@ -12,37 +12,28 @@
 # print('')#PRONTO
 
            #INPUT === PRONTO
-nome=str(input("Digite seu nome: "))
-tel=int(input("Digite seu telefone: "))
-email=str(input("Digite seu e-mail: "))
-insta=str(input("Digite seu usuário no Instagram: "))
-twi=str(input("Digite seu usuário no Twitter: "))
+lista = dict()
 
-if(nome===str){
-    print(nome)
-}else{
+lista   ['Nome']   = str(input("Digite seu nome: "))
+# lista   ['email']  = input("Digite seu e-mail: ")
+# lista   ['insta']  = input("Digite seu usuário no Instagram: ")
+# lista   ['twi']    = (input("Digite seu usuário no Twitter: "))
+# lista   ['tel']    = int(input("Digite seu telefone: "))
+
+if lista.Nome == str:
+    print(lista)
+else:
     nome=str(input("Digite seu nome: ")) 
-}
+
+
 
 #BIBLIOTECA
-lista=[]
-contato1={'nome': 'Alexandre', 'telefone': '85-9898-0000', 'email': 'alexandre@serv.com', 'instagram': 'alex123', 'twitter': '@alex321'}
 
-contato2={'nome': 'Kelly', 'telefone': '85-9898-0001', 'email': 'kelly@serv.com', 'instagram': 'keka123', 'twitter': '@keka321'}
+# contato1={'nome': 'Alexandre', 'telefone': '85-9898-0000', 'email': 'alexandre@serv.com', 'instagram': 'alex123', 'twitter': '@alex321'}
 
-contato3={'nome': 'Vanda', 'telefone': '85-9898-0002', 'email': 'vanda@serv.com', 'instagram': 'vanda123', 'twitter': '@vanda321'}
+# contato2={'nome': 'Kelly', 'telefone': '85-9898-0001', 'email': 'kelly@serv.com', 'instagram': 'keka123', 'twitter': '@keka321'}
 
-contato4={'nome': 'Ricardo', 'telefone': '85-9898-0003', 'email': 'ricardo@serv.com', 'instagram': 'ricardo123', 'twitter': '@ricardo321'}
+# contato3={'nome': 'Vanda', 'telefone': '85-9898-0002', 'email': 'vanda@serv.com', 'instagram': 'vanda123', 'twitter': '@vanda321'}
 
-print(lista)
-
-
-
-#  =   '', ''
-#  =   '85-9898-0002', '85-9898-0003'
-#  =   'vanda@serv.com', 'raimundo@serv.com'
-#  =   'vanda123', 'raimundo123'
-#  = , , '@vanda321', '@raimundo321'
-
-# print(nome)
-# print(telefone)
+# contato4={'nome': 'Ricardo', 'telefone': '85-9898-0003', 'email': 'ricardo@serv.com', 'instagram': 'ricardo123', 'twitter': '@ricardo321'}
+# print(contato1)
