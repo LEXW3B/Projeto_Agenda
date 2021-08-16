f699f69f144900c2ee7712a9fd1d14fd1c8765aa
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-08-16 20:29:29 -0300

    Create dicionario.py

---
+++ py/dicionario.py
@@ -0,0 +1,25 @@
+# print(filme.values()) pega os valores tipo 'star wars', '1977','george lucas'
+# print(filme.keys())pega so os titulo, ano, diretor
+# print(filme.items())pega tudo e imprime na tela 
+# k == keys, v == value, i == items
+
+# pessoas = {'nome': 'gustavo', 'sexo': 'm', 'idade': '22'     }
+# print(pessoas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
